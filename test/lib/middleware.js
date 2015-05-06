57fdd0e1ce710ff1e18b18852d536fa729f36b90
Scott Ganyo <scott@ganyo.com>
2015-05-06 11:21:57 -0700

    switch from yamljs to js-yaml and fix tests

--- test/lib/middleware.js
+++ test/lib/middleware.js
@@ -26,8 +26,6 @@
 var should = require('should');
 var path = require('path');
 var _ = require('lodash');
-var YAML = require('yamljs');
-var util = require('util');
 
 var SwaggerRunner = require('../..');
 
@@ -38,14 +36,9 @@ var DEFAULT_PROJECT_CONFIG = {
   docEndpoints: { raw: '/swagger' }
 };
 
-describe('connect_middleware', function() {
+describe('hapi_middleware', function() {
 
-  function shouldBeConnectMiddleware(middleware) {
-    middleware.should.be.a.Function;
-    middleware.length.should.eql(3);
-  }
-
-  var connectMiddleware, createdRunner;
+  var hapiMiddleware, createdRunner;
 
   before(function(done) {
 
@@ -54,7 +47,7 @@ describe('connect_middleware', function() {
 
       should.exist(mw);
       createdRunner = mw.runner;
-      connectMiddleware = mw;
+      hapiMiddleware = mw;
 
       done();
     });
@@ -64,308 +57,75 @@ describe('connect_middleware', function() {
 
     it('should expose config', function() {
 
-      connectMiddleware.sysConfig.should.be.an.Object;
+      hapiMiddleware.sysConfig.should.be.an.Object;
       _.each(DEFAULT_PROJECT_CONFIG, function(value, key) {
-        connectMiddleware.sysConfig[key].should.eql(value);
+        hapiMiddleware.sysConfig[key].should.eql(value);
       });
     });
 
     it('should expose runner', function() {
 
-      connectMiddleware.runner.should.equal(createdRunner);
+      hapiMiddleware.runner.should.equal(createdRunner);
     });
 
-    it('should expose middleware functions', function() {
+    it('should expose plugin', function() {
 
-      ['metadata', 'security', 'validator', 'router', 'expressCompatibilityMW',
-        'swaggerDoc', 'helpers', 'cors'].forEach(function(funcName) {
-          shouldBeConnectMiddleware(connectMiddleware[funcName]());
-        });
-
-      //todo: connectMiddleware.register();
-
+      should.exist(hapiMiddleware.plugin.register);
+      hapiMiddleware.plugin.register.should.be.a.Function;
+      hapiMiddleware.plugin.register.attributes.should.have.keys('name', 'version');
     });
 
     it('should set config to config.swagger', function() {
 
-     connectMiddleware.sysConfig.should.eql(connectMiddleware.runner.config.swagger);
+      hapiMiddleware.sysConfig.should.eql(hapiMiddleware.runner.config.swagger);
     })
   });
 
-  describe('stack', function() {
+  describe('register', function() {
 
-    it('should return a default Array', function() {
-
-      connectMiddleware.stack().should.be.an.Array
-      connectMiddleware.stack().length.should.eql(7); // note: includes swaggerDoc MW
-
-      connectMiddleware.stack().forEach(function(ea) {
-        shouldBeConnectMiddleware(ea);
-      });
-    });
-
-    // todo: config variations
-
-  });
-
-
-  describe('jsonErrorHandler', function() {
-
-    var jsonErrorHandler;
-
-    before(function() {
-      jsonErrorHandler = connectMiddleware.jsonErrorHandler();
-    });
-
-    it('should be a connect middleware error handler', function() {
-
-      jsonErrorHandler.should.be.a.Function;
-      jsonErrorHandler.length.should.eql(4);
-    });
-
-    it('should emit appropriate json', function(done) {
-
-      var err = new Error('this is a test');
-      var res = {
-        end: function(json) {
-          json.should.eql(JSON.stringify({ message: err.message }))
-          done();
+    it('should call app.register with plugin', function(done) {
+      var registerCalled = false;
+      var app = {
+        register: function(plugin, cb) {
+          should.exist(plugin);
+          plugin.register.should.be.a.Function;
+          registerCalled = true;
+          cb();
         }
       };
-      jsonErrorHandler(err, null, res, function() {
-        should.fail; // should never get here
-      });
-    });
-
-    it('should do nothing if accidentally run as normal middleware', function(done) {
-
-      jsonErrorHandler(null, null, function(err) {
-        should.not.exist(err);
+      app.register(app, function() {
+        registerCalled.should.be.true;
         done();
       });
     });
-
   });
 
-  describe('expressCompatibility', function() {
+  describe('plugin', function() {
 
-    var expressMW;
-    var url = 'http://localhost:10010/test?query1=val1&query2=val2';
+    it('register should register with hapi', function(done) {
 
-    before(function() {
-      expressMW = connectMiddleware.expressCompatibilityMW();
-    });
+      var extCalled = false;
+      var server = {
 
-    it('should add missing properties to request and response', function(done) {
-
-      var requestProps = ['path', 'query', 'get'];
-      var responseProps = ['json', 'get', 'set', 'status'];
-
-      var request = { url: url };
-      var response = {};
-
-      expressMW(request, response, function() {
-        request.should.have.properties(requestProps);
-        response.should.have.properties(responseProps);
-
-        done();
-      });
-    });
-
-    it('should properly handle json()', function(done) {
-
-      var setHeaderCalled = false;
-      var testObject = { this: 'is', a: 'test' };
-
-      var request = { url: url };
-      var response = {
-        setHeader: function(name, value) {
-          name.should.eql('Content-Type');
-          value.should.eql('application/json');
-          setHeaderCalled = true;
+        ext: function(event, funct) {
+          extCalled = true;
+          'onRequest'.should.eql(event);
+          should(funct).be.a.Function;
         },
-        end: function(value) {
-          setHeaderCalled.should.be.true;
-          response.statusCode.should.eql(200);
-          JSON.parse(value).should.eql(testObject);
+
+        on: function(event, funct) {
+          extCalled.should.be.true;
+          'request-error'.should.eql(event);
+          should(funct).be.a.Function;
           done();
         }
       };
 
-      expressMW(request, response, function() {
-        response.json(testObject);
+      hapiMiddleware.plugin.register(server, null, function() {
         should.fail; // should never get here
       });
     });
 
-    it('request.get should get a header', function(done) {
-
-      var request = { url: url, headers: { myheader: 'myvalue' } };
-      var response = {};
-
-      should.not.exist(request.get);
-      expressMW(request, response, function() {
-        request.get('myheader').should.eql(request.headers['myheader']);
-        done();
-      });
-    });
-
-    it('request.status should set status', function(done) {
-
-      var request = { url: url };
-      var response = {};
-
-      should.not.exist(response.statusCode);
-      expressMW(request, response, function() {
-        response.status(200);
-        response.statusCode.should.eql(200);
-        done();
-      });
-    });
-  });
-
-  describe('swaggerDoc', function() {
-
-    var swagger, swaggerDocMW, yaml, json;
-
-    before(function() {
-      swagger = connectMiddleware.runner.swagger;
-      swaggerDocMW = connectMiddleware.swaggerDoc();
-      var filteredSwagger = _.cloneDeep(swagger);
-      delete(filteredSwagger.paths['/hello']['x-swagger-router-controller']);
-      yaml = YAML.stringify(filteredSwagger, 99, 2);
-      json = JSON.stringify(filteredSwagger, null, 2);
-    });
-
-    it('should retrieve swagger json', function(done) {
-
-      var request = {
-        path: connectMiddleware.runner.config.swagger.docEndpoints.raw,
-        headers: { }
-      };
-      var setHeaderCalled = false;
-      var response = {
-        setHeader: function(name, value) {
-          name.should.eql('Content-Type');
-          value.should.eql('application/json');
-          setHeaderCalled = true;
-        },
-        end: function(value) {
-          setHeaderCalled.should.be.true;
-          should(json == value).be.true
-          done();
-        }
-      };
-
-      swaggerDocMW(request, response, function() {
-        should.fail; // should never get here
-      });
-    });
-
-    it('should retrieve swagger yaml', function(done) {
-
-      var request = {
-        path: connectMiddleware.runner.config.swagger.docEndpoints.raw,
-        headers: { accept: 'application/yaml' }
-      };
-      var setHeaderCalled = false;
-      var response = {
-        setHeader: function(name, value) {
-          name.should.eql('Content-Type');
-          value.should.eql('application/yaml');
-          setHeaderCalled = true;
-        },
-        end: function(value) {
-          setHeaderCalled.should.be.true;
-          should(yaml == value).be.true;
-          done();
-        }
-      };
-
-      swaggerDocMW(request, response, function() {
-        should.fail; // should never get here
-      });
-    });
-
-    it('should derive the path as needed', function(done) {
-
-      var request = {
-        url: util.format('http://localhost:10010%s', connectMiddleware.runner.config.swagger.docEndpoints.raw),
-        headers: { accept: 'application/yaml' }
-      };
-      var response = {
-        setHeader: function(name, value) {
-        },
-        end: function(value) {
-          should(yaml == value).be.true;
-          done();
-        }
-      };
-
-      swaggerDocMW(request, response, function() {
-        should.fail; // should never get here
-      });
-    });
-
-    it('should be able to set the filter', function(done) {
-
-      var config = _.clone(DEFAULT_PROJECT_CONFIG);
-      config.docEndpoints = { raw: '/swagger', filter: '.*' };
-      should.exist(swagger.paths['/hello']['x-swagger-router-controller']);
-      var yaml = YAML.stringify(swagger, 99, 2);
-
-      SwaggerRunner.create(config, function(err, connectMiddleware) {
-        should.not.exist(err);
-
-        swaggerDocMW = connectMiddleware.swaggerDoc();
-
-        var request = {
-          path: connectMiddleware.runner.config.swagger.docEndpoints.raw,
-          headers: { accept: 'application/yaml' }
-        };
-        var setHeaderCalled = false;
-        var response = {
-          setHeader: function(name, value) {
-            name.should.eql('Content-Type');
-            value.should.eql('application/yaml');
-            setHeaderCalled = true;
-          },
-          end: function(value) {
-            setHeaderCalled.should.be.true;
-            should(yaml == value).be.true;
-            done();
-          }
-        };
-
-        swaggerDocMW(request, response, function() {
-          should.fail; // should never get here
-        });
-      });
-    });
-
-  });
-
-  describe('helpers', function() {
-
-    var helperMW;
-
-    before(function() {
-      helperMW = connectMiddleware.helpers();
-    });
-
-    it('should add function to get config to request', function(done) {
-
-      var request = {};
-
-      helperMW(request, null, function() {
-        should.exist(request.swagger);
-        should.exist(request.swagger.config);
-        request.swagger.config.should.equal(connectMiddleware.runner.config);
-
-        done();
-      });
-    });
-
-  });
+  })
 
 });
